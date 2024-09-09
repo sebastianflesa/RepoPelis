b00ba92dd3ef3c66fbc1456a410217fc99911ba4
sebastianflesa <sebastian.orrego@fleischmann.cl>
2024-09-09 00:15:53 -0300

    endpoints crud

--- src/main/java/com/cine/demo/controller/PeliculasController.java
+++ src/main/java/com/cine/demo/controller/PeliculasController.java
@@ -5,8 +5,12 @@ import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +40,21 @@ public class PeliculasController {
             return ResponseEntity.status(200).body(map);
         }
     }
+
+    @PutMapping("/{id}")
+    public Pelicula updatePelicula(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+        return peliculaService.updatePelicula(id, pelicula);
+    }
+
+    @PostMapping 
+    public Pelicula createPelicula(@RequestBody Pelicula pelicula) {
+        return peliculaService.createPelicula(pelicula);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePelicula(@PathVariable Long id) {
+        peliculaService.deletePelicula(id);
+    }
+
 
 }
--- src/main/java/com/cine/demo/service/PeliculaService.java
+++ src/main/java/com/cine/demo/service/PeliculaService.java
@@ -9,5 +9,7 @@ import com.cine.demo.model.Pelicula;
 public interface PeliculaService {
     List<Pelicula> getAllPeliculas();
     Optional<Pelicula> getPeliculaById(Long id);
-
+    Pelicula createPelicula(Pelicula pelicula);
+    Pelicula updatePelicula(Long id, Pelicula pelicula);
+    void deletePelicula(Long id);
 }
